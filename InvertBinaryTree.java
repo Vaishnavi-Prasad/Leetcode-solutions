54f195d58c16bb9985c0497f30c165850775764a
Vaishnavi-Prasad <65669865+Vaishnavi-Prasad@users.noreply.github.com>
2020-06-10 15:58:52 +0530

    Add files via upload

---
+++ InvertBinaryTree.java
@@ -0,0 +1,15 @@
+package com.ds.challenges;
+
+public class InvertBinaryTree {
+
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null) {
+                return null;
+            }
+            TreeNode right = invertTree(root.right);
+            TreeNode left = invertTree(root.left);
+            root.left = right;
+            root.right = left;
+            return root;
+        }
+}
