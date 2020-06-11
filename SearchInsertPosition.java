7bc21109c0580e87bb23456840688e15b08c8a6b
Vaishnavi-Prasad <65669865+Vaishnavi-Prasad@users.noreply.github.com>
2020-06-11 11:27:25 +0530

    Search insert position challenge implementation

---
+++ SearchInsertPosition.java
@@ -0,0 +1,29 @@
+package com.ds.challenges;
+
+public class SearchInsertPosition {
+
+        public int searchInsert(int[] nums, int target) {
+            int start = 0;
+            int end = nums.length - 1;
+
+            while (start + 1 < end) {
+                int mid = (start + end) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                } else if(nums[mid] < target) {
+                    start = mid;
+                } else {
+                    end = mid;
+                }
+            }
+
+            if (nums[end] < target) {
+                return end + 1;
+            } else if (nums[start] >= target) {
+                return start;
+            } else {
+                return end;
+            }
+        }
+
+}
